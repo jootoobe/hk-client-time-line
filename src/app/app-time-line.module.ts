5990fa24a5647e243a29cb91e8da632138da15d9
Gabriel Velloso <gavelloso@gmail.com>
2024-02-26 15:22:41 -0300

    yesye

--- src/app/app-time-line.module.ts
+++ src/app/app-time-line.module.ts
@@ -5,6 +5,7 @@ import { AppTimeLineComponent } from './app-time-line.component';
 import { createCustomElement } from '@angular/elements';
 import { AppTimeLineRoutingModule } from './app-time-line.routing';
 import { TimeLineModule } from './pages/containers/time-line/time-line.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TimeLineModule } from './pages/containers/time-line/time-line.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppTimeLineRoutingModule,
     TimeLineModule
 
