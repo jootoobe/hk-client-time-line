bbf4c64a3021a6d745396e850d567606b7542ad3
Gabriel Velloso <gavelloso@gmail.com>
2024-02-27 11:06:10 -0300

    remove {ngZone: 'noop'}

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppTimeLineModule } from './app/app-time-line.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppTimeLineModule, {ngZone: 'noop'})
+platformBrowserDynamic().bootstrapModule(AppTimeLineModule)
   .catch(err => console.error(err));
