e7b52894afe1d30e42362ecf14ef1c839fdbbe7c
Gabriel Velloso <gavelloso@gmail.com>
2024-03-29 14:51:48 -0300

    Alterando url

--- src/app/spider-share/services/iam/auth/sign-in.service.ts
+++ src/app/spider-share/services/iam/auth/sign-in.service.ts
@@ -144,7 +144,7 @@ export class SignInService {
     // Teste autenticação apis
   // Após autenticar é chamado o GET para testar o AuthGruard da api
   getHelloWorld(): Observable<any> {
-    return this.http.get(this.API_TIME_LINE)
+    return this.http.get(this.API_TIME_LINE+'/get-hello')
   }
 
 }
