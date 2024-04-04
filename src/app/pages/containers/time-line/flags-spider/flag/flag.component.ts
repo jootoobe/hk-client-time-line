7584e3053373aeb6eb2ae1d366b2333f90e0924a
Gabriel Velloso <gavelloso@gmail.com>
2024-04-04 18:25:49 -0300

    Setando estilo flags

--- src/app/pages/containers/time-line/flags-spider/flag/flag.component.ts
+++ src/app/pages/containers/time-line/flags-spider/flag/flag.component.ts
@@ -19,13 +19,28 @@ export class FlagComponent implements OnInit, OnChanges, AfterViewInit {
           flag_title: "FLAG1",
           flag_description: "wwwwwww",
           flag_style: 1,
+
           color_hex: "#90ab3d",
           color_rgb: "144, 171, 64",
           color_hsl: "75, 46%, 92%",
+
           flag_local_zone: "America/Sao_Paulo",
           flag_created_at: "Feb 16, 2024, 9:09:03 PM",
           flag_update_at: "0",
           flag_margin_right: '3',
+
+          flag_design: {
+            color_text: 'black',
+            color_nets: {
+              background: 'black',
+              text: 'white'
+            },
+            color_transparency: '0.5',
+            color_hex: "#90ab3d",
+            color_rgb: "144, 171, 64",
+            color_hsl: "75, 46%, 92%"
+          },
+
           date_obj: {
             day_month_year: "2024-02-17T12:00:00.000Z",
             date_origin: "2024-02-17T03:00:00.000Z",
@@ -54,6 +69,20 @@ export class FlagComponent implements OnInit, OnChanges, AfterViewInit {
               flag_created_at: "Feb 16, 2024, 9:09:03 PM",
               flag_update_at: "0",
               flag_margin_right: '0',
+
+              flag_design: {
+                color_text: 'black',
+                color_nets: {
+                  background: 'black',
+                  text: 'white'
+                },
+                color_transparency: '0.2',
+                color_hex: "#90ab3d",
+                color_rgb: "144, 171, 64",
+                color_hsl: "75, 46%, 92%"
+              },
+
+
               date_obj: {
                 day_month_year: "2024-02-17T12:00:00.000Z",
                 date_origin: "2024-02-17T03:00:00.000Z",
@@ -73,94 +102,6 @@ export class FlagComponent implements OnInit, OnChanges, AfterViewInit {
 
 
 
-
-        {
-          year: "2024",
-          flag_id: "flag_id_21ba-bc46-cacd-1156-822d",
-          flag_title: "uuuuuuuuuuuuuu",
-          flag_description: "BBBBBBB",
-          flag_style: 1,
-          color_hex: "#90ab3d",
-          color_rgb: "144, 171, 64",
-          color_hsl: "75, 46%, 92%",
-          flag_local_zone: "America/Sao_Paulo",
-          flag_created_at: "Feb 16, 2024, 9:09:03 PM",
-          flag_update_at: "0",
-          flag_margin_right: '3',
-          date_obj: {
-            day_month_year: "2024-02-17T12:00:00.000Z",
-            date_origin: "2024-02-17T03:00:00.000Z",
-            day: "17",
-            month: "02",
-            month_s: "FEV",
-            year: "2024",
-            month_code: 2,
-            timestamp: 1708171300000,
-            time: "12:00:00"
-          },
-          social_medias_chips: [{ name: 'Facebook' }],
-          subject_tags: [],
-
-          flags2: [
-            {
-              year: "2024",
-              flag_id: "flag_id_21ba-bc46-cacd-1156-822d",
-              flag_title: "uuuuuuu222222",
-              flag_description: "BBBBBBB",
-              flag_style: 2,
-              color_hex: "#90ab3d",
-              color_rgb: "144, 171, 64",
-              color_hsl: "75, 46%, 92%",
-              flag_local_zone: "America/Sao_Paulo",
-              flag_created_at: "Feb 16, 2024, 9:09:03 PM",
-              flag_update_at: "0",
-              flag_margin_right: '0',
-              date_obj: {
-                day_month_year: "2024-02-17T12:00:00.000Z",
-                date_origin: "2024-02-17T03:00:00.000Z",
-                day: "17",
-                month: "02",
-                month_s: "FEV",
-                year: "2024",
-                month_code: 2,
-                timestamp: 1708171300000,
-                time: "12:00:00"
-              },
-              social_medias_chips: [{ name: 'Facebook' }],
-              subject_tags: [],
-            }
-          ],
-        },
-
-
-
-        {
-          year: "2030",
-          flag_id: "flag_id_21ba-bc46-cacd-1156-822d",
-          flag_title: "uuuuuuuuuuuuuu",
-          flag_description: "BBBBBBB",
-          flag_style: 1,
-          color_hex: "#90ab3d",
-          color_rgb: "144, 171, 64",
-          color_hsl: "75, 46%, 92%",
-          flag_local_zone: "America/Sao_Paulo",
-          flag_created_at: "Feb 16, 2024, 9:09:03 PM",
-          flag_update_at: "0",
-          flag_margin_right: '0',
-          date_obj: {
-            day_month_year: "2024-02-17T12:00:00.000Z",
-            date_origin: "2024-02-17T03:00:00.000Z",
-            day: "17",
-            month: "02",
-            month_s: "FEV",
-            year: "2030",
-            month_code: 2,
-            timestamp: 1708171500000,
-            time: "12:00:00"
-          },
-          social_medias_chips: [{ name: 'Facebook' }],
-          subject_tags: []
-        },
 
       ],
     }
@@ -189,8 +130,8 @@ export class FlagComponent implements OnInit, OnChanges, AfterViewInit {
 
   setStylesCards(flag1: FlagModel): any {
     let styles = {
-      'border-left': '.5rem solid rgba(' + flag1.color_rgb + ',1)',
-      'background-color': 'rgba(' + flag1.color_rgb + ',.2)'
+      'border-left': '.5rem solid rgba(' + flag1.flag_design?.color_rgb + ',1)',
+      'background-color': `rgba(${flag1.flag_design?.color_rgb}, ${flag1.flag_design?.color_transparency})`
     };
     return styles;
   }
