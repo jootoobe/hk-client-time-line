6888bc8793ed257255fe60eab5f44e775c0727c5
Gabriel Velloso <gavelloso@gmail.com>
2024-04-08 15:23:41 -0300

    Criando enviroment dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,18 +2,42 @@ export const environment = {
   production: true,
   helloEnvironment: 'development',
 
-  ApiIam: 'https://iam.spider-share.com/api-iam',
+  ApiIam: 'http://localhost:3200/api-iam',
   ApiIam2: 'http://localhost:3200/api-iam/c', // Get crypto data time-line
 
 
   // API
-  ApiTimeLine: 'https://time-line.spider-share.com/api-time-line',
+  ApiTimeLine: 'http://localhost:3001/api-time-line',
 
 
-  styleSpiderShare: 'https://www.spider-share.com',
+  styleSpiderShare: 'http://localhost:3000',
 
-  urlTranslate: 'https://www.spider-share.com',
+  urlTranslate: 'http://localhost:3000',
 
-  assetsProd: 'https://time-line.spider-share.com', //'http://hk-api-iam-spidershare.us-east-1.elasticbeanstalk.com
+  assetsProd: 'http://localhost:3001', //'http://hk-api-iam-spidershare.us-east-1.elasticbeanstalk.com
 
 };
+
+
+  // // ApiIam: 'http://localhost:3200/api-iam',
+  // ApiIam: 'http://localhost:3200/api-iam', //http://hk-iam-back-spidershare.us-east-1.elasticbeanstalk.com
+  // ApiIam2: 'http://localhost:3200/api-iam/c', // Get crypto data
+
+  // // 🍕 APIS
+  // // SPIDER-SHARE
+  // ApiSpiderShare: 'http://localhost:3000',
+
+  // //TIME-LINE
+  // ApiTimeLine: 'http://localhost:3001',
+
+  // //Kanban
+  // ApiKanban: 'http://localhost:3002',
+
+  // //SPIDER_TUBE
+  // ApiSpiderTube: 'http://localhost:3003',
+
+
+  // // PROJETOS
+  // TIME_LINE: 'http://localhost:3001/app-time-line.js', // http://hk-back-time-line.us-east-1.elasticbeanstalk.com/
+  // KANBAN: 'http://localhost:3002/app-kanban.js',
+  // SPIDER_TUBE: 'http://localhost:3003/app-spider-tube.js',
