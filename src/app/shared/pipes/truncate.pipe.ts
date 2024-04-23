a76f8b13a7f6e75b71796e07bc8e41fe18200621
Gabriel Velloso <gavelloso@gmail.com>
2024-04-23 14:36:08 -0300

    corrigindo pipe

--- src/app/shared/pipes/truncate.pipe.ts
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -3,9 +3,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'truncate' })
 export class TruncatePipe implements PipeTransform {
 
-  transform(value: string, limit: number): string {
-    if(value) {
+  transform(value: string, limit: number, name?: string): string {
+
+    if(value && !name) {
       return value.length < limit  ? value : value.slice(0, limit) + '...';
+    }
+    else if(value && name === 'chip') {
+      return value.length < limit  ? value : value.slice(0, limit);
     }
     return value
   }
