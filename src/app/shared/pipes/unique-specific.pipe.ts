bcee4633fba8f1cdd4c607cc1149de4df881ab8e
Gabriel Velloso <gavelloso@gmail.com>
2024-04-25 14:03:30 -0300

    Caminhando

--- src/app/shared/pipes/unique-specific.pipe.ts
+++ src/app/shared/pipes/unique-specific.pipe.ts
@@ -9,14 +9,10 @@ export class UniqueSpecificPipe implements PipeTransform {
 
     let uniqueArr: any = [];
     let allCode = items.map((value) => value['flag_design'][term]);
-    console.log('111111111111111111',allCode)
     items.map((value, index) => {
       if (allCode.indexOf(value['flag_design'][term]) !== index) {
-        //   // we don't don't need 'Title' in case it is duplicate
-        // value.flag_design.color_hex = undefined
-        value.flag_design.color_hex = 'undefined'
-        // console.log(allCode)
-        console.log(value)
+        value.flag_design.color_hex = undefined
+        delete value.flag_design.color_hex
         uniqueArr.push(value)
       } else {
         uniqueArr.push(value)
