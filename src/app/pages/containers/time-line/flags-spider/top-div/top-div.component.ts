170f7dce9f360c5936c79c2e22b89c7331c505ba
Gabriel Velloso <gavelloso@gmail.com>
2024-04-29 10:12:19 -0300

    Add comentário

--- src/app/pages/containers/time-line/flags-spider/top-div/top-div.component.ts
+++ src/app/pages/containers/time-line/flags-spider/top-div/top-div.component.ts
@@ -15,13 +15,14 @@ export class TopDivComponent implements OnInit {
   @Input({ required: true }) timeLine!: TimeLineModel
   // toda vez que o menu for clicado eu limpoo filtro todo -- O Filtro não pode estar ativo quando o usuário edita ou deleta a bandeira 
   @Input({ required: true }) resetFilterInput!: Boolean 
+  // <!-- Filter opacity -->
   @Input({ required: true }) valFilterColorBarInput = { color_hex: '', color_rgb: 0 } as any // stores the clicked filter bar and communicates with the top-div component
   timeLineOutput = output<TimeLineModel>()
   openModalOutput = output()
   clearBarFilterBarOutput = output() // limpa o filtro barra inferior
 
   toApplyFilterText = ''
-  toApplyFilterColor = [] as any
+  toApplyFilterColor = [] as any //  <!-- Filter select -->
 
   constructor(
     // private connectingExternalRoutesService: ConnectingExternalRoutesService,
