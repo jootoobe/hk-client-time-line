26313e251c083dcb406ec49bdec334508f48ff28
Gabriel Velloso <gavelloso@gmail.com>
2024-05-02 10:15:58 -0300

    TolltipModalSoublecheckerHelper

--- src/app/components/modal-doublechecker/tolltip-modal-doublechecker-helper.ts
+++ src/app/components/modal-doublechecker/tolltip-modal-doublechecker-helper.ts
@@ -33,7 +33,7 @@ export class TolltipModalSoublecheckerHelper {
       <p style='margin-top: 1rem;'><span style='margin-left: .2rem;'>${msn1_c}</span></p>
     </div>
     <p style='margin: 1rem;'>
-    <img style='width: 29rem' src="${img1_a}" alt='SpiderShare' />
+    <img style="height: auto;width:35rem;"  src="${img1_a}" alt='SpiderShare' />
     </p>
     `
   }
