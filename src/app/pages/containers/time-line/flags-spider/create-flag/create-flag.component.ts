7dac296030b032499b6d63a48d445f91002f9421
Gabriel Velloso <gavelloso@gmail.com>
2024-08-16 10:13:20 -0300

    Seguir daqui ajuste data

--- src/app/pages/containers/time-line/flags-spider/create-flag/create-flag.component.ts
+++ src/app/pages/containers/time-line/flags-spider/create-flag/create-flag.component.ts
@@ -943,8 +943,9 @@ export class CreateFlagComponent implements OnChanges, OnInit, AfterViewInit {
       flags.at(0)?.get('flag_design')?.get('color_hsl')?.setValue(colorFormats.hsl)
 
       // Toda vez que a data for alterada ou a cor da bandeira 02, o fundo da data será resetado para ficar igual ao da bandeira 01.
-      if (this.editFlagFormInput && this.editFlagFormInput.flags2 && this.editFlagFormInput.flags2?.length > 0) {
-        // this.editFlagFormInput.flags2[0].flag_design.color_date = colorFormats.rgb
+      if (val2 === 'input' && this.editFlagFormInput && this.editFlagFormInput.flags2 && this.editFlagFormInput.flags2?.length > 0) {
+        this.editFlagFormInput.flags2[0].flag_design.color_date = colorFormats.rgb
+        val2 = ''
       }
     }
     this.colorHexaVal = flags.at(0)?.get('flag_design')?.get('color_hex')?.value
